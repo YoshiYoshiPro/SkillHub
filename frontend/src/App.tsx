ace4ca9a348fa884d3c443ea185349593c67814a
徳持進一 <>
2023-09-05 18:30:17 +0900

    簡単なヘッダーの作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header"
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
