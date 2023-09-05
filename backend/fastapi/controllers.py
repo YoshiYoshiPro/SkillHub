c4af718713b0a3fe8d78b763227e107b1c2329e8
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2023-09-06 01:11:56 +0900

    feat: ✨ Auth0の設定を追加

--- backend/fastapi/controllers.py
+++ backend/fastapi/controllers.py
@@ -1,16 +1,36 @@
+import os
+
 import uvicorn
+from authlib.integrations.starlette_client import OAuth
+from dotenv import load_dotenv
+from fastapi import FastAPI
 from starlette.requests import Request
 
-from fastapi import FastAPI
+app = FastAPI(title="SkillHub", description="社内の勉強会の予定を共有するWebアプリケーション")
 
-app = FastAPI(
-    title='社内勉強会の開催を活発にするwebアプリ',
-    description='社内の勉強会の予定を共有するWebアプリケーション'
+load_dotenv("../.env")  # .envファイルのパスを指定
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+DOMAIN = os.environ.get("DOMAIN")
+
+
+# Auth0の設定
+oauth = OAuth()
+oauth.register(
+    name="auth0",
+    client_id="CLIENT_ID",
+    client_secret="CLIENT_SECRET",
+    authorize_url=f"https://{DOMAIN}/authorize",
+    authorize_params=None,
+    access_token_url=f"https://{DOMAIN}/oauth/token",
+    access_token_params=None,
+    redirect_uri="http://localhost:8000/callback",
+    client_kwargs={"scope": "openid profile email"},
 )
 
 
 def index(request: Request):
-    return {'Hello': 'World'}
+    return {"Hello": "World"}
 
 
 if __name__ == "__main__":
