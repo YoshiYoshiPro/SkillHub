8dffe88364340d6746cf2e1efc838c66c4f589c2
Hikoyann <i21i022dl@ous.jp>
2023-09-05 15:28:09 +0900

    ✨ Profileの作成を行いました。

---
+++ frontend/src/components/Profile.tsx
@@ -0,0 +1,39 @@
+import log1 from "../imges/log1.png";
+function Profile() {
+  // const handleClic = useCallback(() => {
+  //   // a
+  // });
+
+  return (
+    <>
+      <div className="container">
+        <div className="d-flex">
+          <div className="col-4 mt-5">
+            <img
+              src={log1}
+              className="rounded-circle border border-dark"
+              alt="アイコン"
+            />
+          </div>
+          <div className="col-8">
+            <p className="mt-5 mb-1">名前</p>
+            <p className="mb-1">メールアドレス：</p>
+            <p className="mb-1">ひとこと</p>
+
+            <p className="mt-4 mb-1">入社年数</p>
+            <p className="mb-1">所属先</p>
+
+            <p className="mb-1">興味のある技術</p>
+            <p className="mt-4 mb-1">業務経験のある技術</p>
+            <p className="mb-1">得意な技術</p>
+            <div className="d-flex">
+              <button className="ml-auto mr-5 mt-4">編集</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Profile;
