6a4e5df00bcf3ec8a57be2ec5298ab23ca522c1d
Hikoyann <i21i022dl@ous.jp>
2023-09-05 19:09:41 +0900

    ✨ Profileを記入可能にした

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 import Profile from "./components/Profile";
-import ProfileEdit from "./components/ProfileEdit";
+
+// import Login from "./components/Login";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/profileEdit" element={<ProfileEdit />} />
+        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </div>
   );
--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,8 +1,45 @@
-import log1 from "../imges/log1.png";
+import { useCallback, useState } from "react";
+import log1 from "../img/log1.png";
 function Profile() {
-  // const handleClic = useCallback(() => {
-  //   // a
-  // });
+  const [textName, setNameText] = useState("");
+  const [textMail, setMailText] = useState("");
+  const [textJoin, setJoinText] = useState("");
+  const [textWord, setWordText] = useState("");
+  const [textAff, setAffText] = useState("");
+  const [textInterest, setInterestText] = useState("");
+  const [textBusiness, setBusinessText] = useState("");
+  const [textGood, setGoodText] = useState("");
+
+  const nameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setNameText(e.target.value.trim());
+  }, []);
+  const mailChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setMailText(e.target.value.trim());
+  }, []);
+  const joinChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setJoinText(e.target.value.trim());
+  }, []);
+  const wordChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setWordText(e.target.value.trim());
+  }, []);
+  const AffChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setAffText(e.target.value.trim());
+  }, []);
+  const interestChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInterestText(e.target.value.trim());
+    },
+    []
+  );
+  const businessChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setBusinessText(e.target.value.trim());
+    },
+    []
+  );
+  const goodChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setGoodText(e.target.value.trim());
+  }, []);
 
   return (
     <>
@@ -16,18 +53,83 @@ function Profile() {
             />
           </div>
           <div className="col-8">
-            <p className="mt-5 mb-1">名前</p>
-            <p className="mb-1">メールアドレス：</p>
-            <p className="mb-1">ひとこと</p>
+            <div className="mt-5 mb-5 d-flex">
+              <p className=" mb-1">名前</p>
+              <input
+                type="text"
+                className="form-control w-75 ml-auto"
+                value={textName}
+                onChange={nameChange}
+              />
+            </div>
+            <div className="mb-5 d-flex">
+              <p className=" mb-1">メールアドレス:</p>
+              <input
+                type="text"
+                className="form-control w-75 ml-auto"
+                value={textMail}
+                onChange={mailChange}
+              />
+            </div>
+            <div className="mb-5 d-flex">
+              <p className=" mb-1">ひとこと</p>
+              <input
+                type="text"
+                className="form-control w-75 ml-auto"
+                value={textJoin}
+                onChange={wordChange}
+              />
+            </div>
+            <div className="mb-5 d-flex">
+              <p className=" mb-1">入社年数</p>
+              <input
+                type="text"
+                className="form-control w-75 ml-auto"
+                value={textWord}
+                onChange={joinChange}
+              />
+            </div>
+            <div className="mb-5 d-flex">
+              <p className=" mb-1">所属先</p>
+              <input
+                type="text"
+                className="form-control w-75 ml-auto"
+                value={textAff}
+                onChange={AffChange}
+              />
+            </div>
+            <div className="mb-5 d-flex">
+              <p className=" mb-1">興味のある技術</p>
+              <input
+                type="text"
+                className="form-control w-75 ml-auto"
+                value={textInterest}
+                onChange={interestChange}
+              />
+            </div>
+            <div className="mb-5 d-flex">
+              <p className=" mb-1">業務経験のある技術</p>
+              <input
+                type="text"
+                className="form-control w-75 ml-auto"
+                value={textBusiness}
+                onChange={businessChange}
+              />
+            </div>
+            <div className="mb-5 d-flex">
+              <p className=" mb-1">得意な技術</p>
+              <input
+                type="text"
+                className="form-control w-75 ml-auto"
+                value={textGood}
+                onChange={goodChange}
+              />
+            </div>
 
-            <p className="mt-4 mb-1">入社年数</p>
-            <p className="mb-1">所属先</p>
-
-            <p className="mb-1">興味のある技術</p>
-            <p className="mt-4 mb-1">業務経験のある技術</p>
-            <p className="mb-1">得意な技術</p>
             <div className="d-flex">
-              <button className="ml-auto mr-5 mt-4">編集</button>
+              <button className="ml-auto mr-5 mt-4 btn btn-primary">
+                編集
+              </button>
             </div>
           </div>
         </div>
