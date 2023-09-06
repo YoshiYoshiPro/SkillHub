cf078fc037a9c4308add2987187ad0d2f2650372
okamoto <1974336t@stu.kobe-u.ac.jp>
2023-09-06 11:21:17 +0900

    ユーザーテーブルの存在を確認

--- backend/fastapi/controllers.py
+++ backend/fastapi/controllers.py
@@ -13,5 +13,5 @@ def index(request: Request):
     return {'Hello': 'World'}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+def get_data(request: Request):
+    return {"key": "value","message":"Hello from FastAPI!"}
--- backend/fastapi/urls.py
+++ backend/fastapi/urls.py
@@ -2,3 +2,9 @@ from controllers import *
 
 # FastAPIのルーティング用関数
 app.add_api_route('/', index)
+
+# データの受け渡しできるかテスト
+app.add_api_route('/data',get_data)
+
+# # プロフィールの表示
+# app.add_api_route('/get-profile',get_profile)
