acfa6e019daa5a0405a7a273a17f68bca570f791
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2023-09-07 04:42:42 +0900

    feat: ✨ contextを用いてユーザー情報を保持

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,46 @@
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  ReactNode,
+} from "react";
+import { auth } from "../firebase";
+import { User } from "firebase/auth";
+
+interface AuthContextType {
+  user: User | null;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function useAuthContext() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuthContext must be used within an AuthProvider");
+  }
+  return context;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const value = {
+    user,
+  };
+
+  useEffect(() => {
+    const unsubscribed = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return () => {
+      unsubscribed();
+    };
+  }, []);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
