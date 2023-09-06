dccb5c554f042041c7bbcc502856d030cb4fa025
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2023-09-06 12:39:25 +0900

    feat: ✨ authのjwtバックエンド実装完了

--- backend/fastapi/auth.py
+++ backend/fastapi/auth.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 import requests
 from dotenv import load_dotenv
-from fastapi import Depends, HTTPException, Security
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
@@ -18,12 +18,45 @@ ALGORITHMS = ["RS256"]
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
+# キャッシュ変数の初期化
+PUBLIC_KEY_CACHE = {}
+
+
 def get_public_key(kid: str) -> Optional[str]:
-    jwks = requests.get(JWKS_URL).json()
-    for key in jwks["keys"]:
-        if key["kid"] == kid:
-            cert = key["x5c"][0]
-            return f"-----BEGIN CERTIFICATE-----\n{cert}\n-----END CERTIFICATE-----"
+    # キャッシュから公開鍵を取得
+    if kid in PUBLIC_KEY_CACHE:
+        return PUBLIC_KEY_CACHE[kid]
+
+    try:
+        # タイムアウトを設定してリクエストを行う
+        response = requests.get(JWKS_URL, timeout=10)
+
+        # ステータスコードの確認
+        response.raise_for_status()
+
+        jwks = response.json()
+        for key in jwks["keys"]:
+            if key["kid"] == kid:
+                cert = key["x5c"][0]
+                pem_key = (
+                    f"-----BEGIN CERTIFICATE-----\n{cert}\n-----END CERTIFICATE-----"
+                )
+
+                # 公開鍵をキャッシュに保存
+                PUBLIC_KEY_CACHE[kid] = pem_key
+                return pem_key
+    except requests.Timeout:
+        # タイムアウト時のエラーハンドリング
+        raise HTTPException(status_code=500, detail="JWKS endpoint request timed out.")
+    except requests.RequestException as e:
+        # その他のリクエスト関連のエラーハンドリング
+        raise HTTPException(
+            status_code=500, detail=f"Error occurred while fetching JWKS: {e}"
+        )
+    except ValueError:
+        # JSONデコードエラーのハンドリング
+        raise HTTPException(status_code=500, detail="Failed to decode JWKS response.")
+
     return None
 
 
