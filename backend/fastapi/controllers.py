930da8ed5cfbc20c62f8bb8858dcd2fe44188c9f
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2023-09-06 10:57:34 +0900

    fix: 🐛 コンフリクト解消

--- backend/fastapi/controllers.py
+++ backend/fastapi/controllers.py
@@ -1,17 +1,9 @@
 import uvicorn
+from fastapi import FastAPI
 from starlette.requests import Request
 
-from fastapi import FastAPI
-
-app = FastAPI(
-    title='社内勉強会の開催を活発にするwebアプリ',
-    description='社内の勉強会の予定を共有するWebアプリケーション'
-)
+app = FastAPI(title="社内勉強会の開催を活発にするwebアプリ", description="社内の勉強会の予定を共有するWebアプリケーション")
 
 
 def index(request: Request):
-    return {'Hello': 'World'}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    return {"Hello": "World"}
