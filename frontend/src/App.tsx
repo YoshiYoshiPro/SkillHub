4adc904a74bb54def2f07b7b283a1e2b25d462bb
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2023-09-07 04:43:13 +0900

    feat: ✨ ログイン前はhomeからloginへリダイレクト

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,20 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
