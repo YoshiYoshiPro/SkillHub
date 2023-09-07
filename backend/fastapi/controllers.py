e94bbbf8f894e026fedddd542c4d075f2c00ea17
okamoto <1974336t@stu.kobe-u.ac.jp>
2023-09-07 13:06:02 +0900

    upgrade-profileの更新

--- backend/fastapi/controllers.py
+++ backend/fastapi/controllers.py
@@ -1,4 +1,6 @@
-from typing import List
+
+
+from ast import List, Tuple
 
 import uvicorn
 from sqlalchemy.orm import Session
@@ -136,3 +138,82 @@ def create_user(user: schemas.UsersCreate, db: Session = Depends(get_db)):
 def get_all_users(db: Session = Depends(get_db)):
     users = crud.get_all_users(db)  # データベース操作関数を呼び出してデータを取得
     return users
+
+def get_user_profile(user_id: int, db: Session = Depends(get_db)):
+    user = crud.get_user_profile(db, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # プロフィール情報を要求されたフォーマットに整形
+    profile_data = {
+        "name": user.name,
+        "sns_link": user.sns_link,
+        "comment": user.comment,
+        "join_date": str(user.join_date),
+        "department": user.department,
+        "interests": [{"name": interest.name} for interest in user.interests],
+        "expertises": [{"name": expertise.name, "years": expertise.expertise_years} for expertise in user.expertises],
+        "experiences": [{"name": experience.name, "years": experience.experience_years} for experience in user.experiences],
+    }
+    return profile_data
+
+def update_user_profile(
+    user_id: int,
+    edited_sns_link: str,
+    edited_comment: str,
+    edited_join_date: str,
+    edited_department: str,
+    edited_interests: list[int],
+    edited_expertises: List[Tuple[int, int]],
+    edited_experiences: List[Tuple[int, int]],
+    db: Session = Depends(get_db)
+):
+    user_profile = crud.get_user_profile(db,user_id)
+    if user_profile is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_profile.sns_link = edited_sns_link
+    user_profile.comment = edited_comment
+    user_profile.join_date = edited_join_date
+    user_profile.department = edited_department
+
+    # 興味を更新
+    # まず、指定された user_id に関連する現在の user_interests レコードを削除
+    db.query(models.UserInterests).filter(models.UserInterests.user_id == user_id).delete()
+
+    db.query(models.UserExpertises).filter(models.UserExpertises.user_id == user_id).delete()
+
+    db.query(models.UserExperiences).filter(models.UserExperiences.user_id == user_id).delete()
+
+    # 興味を更新
+    for interest_id in edited_interests:
+        user_interest = models.UserInterests(
+            user_id=user_id,
+            technology_id=interest_id,
+            interest_years=1,  # 適切な値を設定してください
+        )
+        db.add(user_interest)
+
+    # 専門性を更新
+    for expertise in edited_expertises:
+        technology_id, expertise_years = expertise
+        user_expertise = models.UserExpertises(
+            user_id=user_id,
+            technology_id=technology_id,
+            expertise_years=expertise_years,
+        )
+        db.add(user_expertise)
+
+
+    # 経験を更新
+    for experience in edited_experiences:
+        technology_id, experience_years = experience
+        user_experience = models.UserExpertises(
+            user_id=user_id,
+            technology_id=technology_id,
+            experience_years=experience_years,
+        )
+        db.add(user_experience)
+
+    db.commit()
+
+    return{"is_accepted":True}
--- backend/fastapi/urls.py
+++ backend/fastapi/urls.py
@@ -15,11 +15,17 @@ app.add_api_route('/search-tag/{tag}', get_tag_result)
 # タグサジェストリクエスト
 app.add_api_route('/get-suggested-tags/{tag_substring}', get_suggested_tags)
 
-# 新しいエンドポイントを追加
+# 指定のuser_idを持つユーザテーブルを取得するリクエスト
 app.add_api_route('/users/{user_id}', get_user_by_id, methods=['GET'])
 
-# 新しいエンドポイントを追加
+# ユーザーテーブルを追加するリクエスト
 app.add_api_route('/users/', create_user, methods=['POST'])
 
-# 新しいエンドポイントを追加
+# ユーザテーブルを全取得するリクエスト
 app.add_api_route('/users/', get_all_users, methods=['GET'])
+
+# 特定のユーザーのプロフィール情報を取得するリクエスト
+app.add_api_route('/get-profile/{user_id}', get_user_profile, methods=['GET'])
+
+# 特定のユーザーのプロフィール情報を編集するリクエスト
+app.add_api_route('/update-profile/{user_id}', update_user_profile, methods=['POST'])
