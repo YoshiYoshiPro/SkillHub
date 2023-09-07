1bda875d6fce50f8f42518b3e776734bf2e8eb3c
Tokumochimochi <tokumochimochi@gmail.com>
2023-09-07 22:42:00 +0900

    コンフリクト消し忘れ

--- backend/fastapi/urls.py
+++ backend/fastapi/urls.py
@@ -14,12 +14,11 @@ app.add_api_route("/data", get_data)
 app.add_api_route("/search-tec/{tec_id}", get_tec_result)
 
 
-<<<<<<< HEAD
-=======
+
 # 技術トレンド取得リクエスト
 app.add_api_route("/get-trend-tecs/", get_trend_tecs)
 
->>>>>>> 8590619247536ab835c215af3bc8c9a91c0a330b
+
 # 技術サジェスト取得リクエスト
 app.add_api_route("/get-suggested-tecs/{tec_substring}", get_suggested_tecs)
 
@@ -36,18 +35,13 @@ app.add_api_route("/users/{user_id}", get_user_by_id, methods=["GET"])
 app.add_api_route("/users/", create_user, methods=["POST"])
 
 # ユーザテーブルを全取得するリクエスト
-<<<<<<< HEAD
-app.add_api_route('/users/', get_all_users, methods=['GET'])
-=======
 app.add_api_route("/users/", get_all_users, methods=["GET"])
 
->>>>>>> 8590619247536ab835c215af3bc8c9a91c0a330b
 
 # 特定のユーザーのプロフィール情報を取得するリクエスト
 app.add_api_route("/get-profile/{user_id}", get_user_profile, methods=["GET"])
 
 # 特定のユーザーのプロフィール情報を編集するリクエスト
-<<<<<<< HEAD
 app.add_api_route('/update-profile/{user_id}', update_user_profile, methods=['POST'])
 
 # 勉強会参加したいリクエスト（いいね機能）
@@ -61,6 +55,3 @@ app.add_api_route('/update-timeline', timeline , methods=['GET'])
 
 # トレンド技術取得リクエスト
 app.add_api_route('/get-trend-tec', get_trend , methods=['GET'])
-=======
-app.add_api_route("/update-profile/{user_id}", update_user_profile, methods=["POST"])
->>>>>>> 8590619247536ab835c215af3bc8c9a91c0a330b
