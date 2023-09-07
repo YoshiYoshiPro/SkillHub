8d635b033fe9f3bc005bf07b186131a8bfdc1ec6
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2023-09-07 11:30:02 +0900

    feat: ✨ userをexportしてヘッダー部分でユーザー情報を取得可能にした

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,25 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
 
 interface SessionSuggestionPost {
-  technology: string,
-  likes: number,
-  date: Date,
+  technology: string;
+  likes: number;
+  date: Date;
 }
 
 function Header() {
+  const { user } = useAuthContext();
 
-  return <header>
-		<div className="d-flex px-3 py-1 border-bottom border-dark">
-			<Link to="/" className='text-decoration-none my-auto'>
-				<h1 className="my-0">SkillHub</h1>
-			</Link>
-			<div className="ml-auto my-auto">
-				<Link to="/profile">
-					<img className='border border-dark rounded-circle' src="http://localhost:3000/logo192.png" width={50}/>
-				</Link>
-			</div>
-		</div>
-  </header>
+  return (
+    <header>
+      <div className="d-flex px-3 py-1 border-bottom border-dark">
+        <Link to="/" className="text-decoration-none my-auto">
+          <h1 className="my-0">SkillHub</h1>
+        </Link>
+        <div className="ml-auto my-auto">
+          <Link to="/profile">
+            <img
+              className="border border-dark rounded-circle"
+              src={user?.photoURL || "http://localhost:3000/logo192.png"}
+              width={50}
+            />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -35,6 +35,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   useEffect(() => {
     const unsubscribed = auth.onAuthStateChanged((user) => {
+      console.log(user);
       setUser(user);
     });
     return () => {
@@ -43,4 +44,12 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }, []);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 }
